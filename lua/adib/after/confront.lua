9810968f52b358b4387e9998e66f5cd3e748f611
Abrar Fahim <adibabrarfahim@gmail.com>
2024-08-23 12:05:30 +0600

    Added formatting

--- lua/adib/after/confront.lua
+++ lua/adib/after/confront.lua
@@ -2,11 +2,6 @@ require("conform").setup({
   formatters_by_ft = {
     cpp = {"clang-format"}
   },
-  format_on_save = {
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500
-  }
 })
 
 
