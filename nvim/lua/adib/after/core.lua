c2cdf50c9ff99cb6db8ace1e3e4b2f190c906df2
shineki-shinx <adibabrarfahim@gmail.com>
2024-12-04 21:54:38 +0600

    Fixes and new stuff

--- nvim/lua/adib/after/core.lua
+++ nvim/lua/adib/after/core.lua
@@ -25,10 +25,10 @@ vim.cmd[[:let g:netrw_browsex_viewer= "chrome"]]
 
 
 function Transparent(color)
-color = color or "moonfly"
+color = color or "duskfox"
  vim.cmd.colorscheme(color)
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.cmd[[hi LineNr guibg=NONE]]
 end
 Transparent()
